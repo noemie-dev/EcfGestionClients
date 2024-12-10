fdc688aca09861a087464c9147815dfec67d106b
noemie-dev <nojnk.nonws@gmail.com>
2024-12-10 18:42:00 +0100

    Correction Prospect tostring et try catch, preparation comparaison raison sociale

--- src/Main.java
+++ src/Main.java
@@ -29,10 +29,10 @@ public class Main {
     public static void remplissage() {
         try {
 
-            Client client = new Client(0, "bob", new Adresse("5", "liberation", "57670", "bobville"), "+3385562012", "email@email.com", "non", 5454, 55);
-            Client client2 = new Client(0, "bob", new Adresse("5", "liberation", "57670", "bobville"), "+3385562012", "email@email.com", "non", 5454, 55);
-            Prospect prospect = new Prospect(0, "bob", new Adresse("5", "liberation", "57670", "bobville"), "+3385562012", "email@email.com", " ", "27/08/1995", "OUI");
-            Prospect prospect2 = new Prospect(0, "bob", new Adresse("5", "liberation", "57670", "bobville"), "+3385562012", "email@email.com", "non", "27/08/1995", "non");
+            Client client = new Client(0, "apple", new Adresse("5", "liberation", "57670", "bobville"), "+3385562012", "email@email.com", "non", 5454, 55);
+            Client client2 = new Client(0, "microsoft", new Adresse("5", "liberation", "57670", "bobville"), "+3385562012", "email@email.com", "non", 5454, 55);
+            Prospect prospect = new Prospect(0, "google", new Adresse("5", "liberation", "57670", "bobville"), "+3385562012", "email@email.com", " ", "27/08/1995", "OUI");
+            Prospect prospect2 = new Prospect(0, "linux", new Adresse("5", "liberation", "57670", "bobville"), "+3385562012", "email@email.com", "non", "27/08/1995", "non");
             System.out.println(client2);
             System.out.println(client);
             System.out.println(prospect);
