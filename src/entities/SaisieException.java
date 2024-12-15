9e73e4b08a431f777014884399da3781da69ed00
noemie-dev <nojnk.nonws@gmail.com>
2024-12-15 14:13:55 +0100

    Commentaires Javadoc ok

--- src/entities/SaisieException.java
+++ src/entities/SaisieException.java
@@ -2,7 +2,6 @@ package entities;
 
 /** classe d'exception personnalisée pour relever les problèmes liés à la saisie de l'utilisateur, heritant de la classe Exception
  * */
-
 public class SaisieException extends Exception {
     public SaisieException(String message) {
         super(message);
