a1f619510e42e05df9509844344dfb43e15b395b
noemie-dev <nojnk.nonws@gmail.com>
2025-02-13 20:38:34 +0100

    DAOClient en cours, exceptions et DAOException à mettre en gravité

--- src/DAO/DaoClient.java
+++ src/DAO/DaoClient.java
@@ -1,10 +1,14 @@
 package DAO;
 
 import com.mysql.cj.xdevapi.Client;
+import entities.Adresse;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DaoClient {
     private Connection connection;
@@ -14,7 +18,7 @@ public class DaoClient {
     }
 
     // 3. Insérer un enregistrement (create)
-    public boolean create(Client client) throws SQLException {
+/*    public boolean create(Client client) throws SQLException {
         String query = "INSERT INTO clients (nom, email) VALUES (?, ?)";
         try (PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
             stmt.setString(1, client.getNom());
@@ -22,7 +26,54 @@ public class DaoClient {
 
             int affectedRows = stmt.executeUpdate();
             return affectedRows > 0;
+        }*/
+
+        public List<Client> findAll() throws SQLException {
+            List<Client> clients = new ArrayList<>();
+            PreparedStatement findAllStmt = null;
+            ResultSet rs = null;
+
+            String findAllQuery = "SELECT * FROM Client";
+
+            try {
+                connection.setAutoCommit(false);
+                findAllStmt = connection.prepareStatement(findAllQuery);
+                rs = findAllStmt.executeQuery();
+
+                while (rs.next()) {
+                    Client client = new Client(
+                            rs.getString("raisonSociale"),
+                            new Adresse(rs.getString("rue"), rs.getString("ville"), rs.getString("codePostal")),
+                            rs.getString("telephone"),
+                            rs.getString("email"),
+                            rs.getString("commentaire"),
+                            rs.getLong("chiffreAffaires"),
+                            rs.getInt("nbrEmployes")
+                    );
+                    clients.add(client);
+                }
+
+                connection.commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                if (connection != null) {
+                    try {
+                        System.err.println("Transaction is being rolled back");
+                        connection.rollback();
+                    } catch (SQLException excep) {
+                        excep.printStackTrace();
+                    }
+                }
+            } finally {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (findAllStmt != null) {
+                    findAllStmt.close();
+                }
+                connection.setAutoCommit(true);
+            }
+
+            return clients;
         }
     }
-
-}
