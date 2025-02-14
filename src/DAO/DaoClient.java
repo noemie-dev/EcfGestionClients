64c9f54c8dacdebae92b9f84e3ad636131937d5c
CDA-03 <nojnk.nonws @gmail.com>
2025-02-14 14:24:48 +0100

    findall ok

--- src/DAO/DaoClient.java
+++ src/DAO/DaoClient.java
@@ -1,4 +1,6 @@
 package DAO;
+import entities.Adresse;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,8 +15,18 @@ public class DaoClient {
             ps = con.prepareStatement(query);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
+                Integer idClient = rs.getInt("idClient");
                 String raisonSociale = rs.getString("raisonSociale");
-                System.out.println(raisonSociale);
+                String rueNb = rs.getString("rueNb");
+                String rueNom = rs.getString("rueNom");
+                String codePostal = rs.getString("codePostal");
+                String ville = rs.getString("ville");
+                String telephone = rs.getString("telephone");
+                String mail = rs.getString("mail");
+                String commentaires = rs.getString("commentaires");
+                String chiffreAffaires = rs.getString("chiffreAffaires");
+                String nbrEmployes = rs.getString("nbrEmployes");
+                System.out.println(idClient+raisonSociale+rueNb+rueNom+codePostal+chiffreAffaires+nbrEmployes+commentaires+mail+ville);
             }
         } catch (SQLException e) {
             e.printStackTrace();
